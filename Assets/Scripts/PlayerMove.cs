baa3d66c22b2496c8ff044dc2e3af41d321c9292
tonevx <anthonysloat02@gmail.com>
2026-09-13 14:37:36 -0400

    Delivery Dash Lesson 5 (2.6)
    Setup for the actual movement of the car is added, on top of cleaning up some unneeded comments in the code itself.

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,21 +1,38 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Layouts;
 
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 0.89f;
     [SerializeField] float moveSpeed = 0.2f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
     
-    // Update is called once per frame
     void Update()
     {
+        if(Keyboard.current.wKey.isPressed)
+        {
+            Debug.Log("Pressing the W Key");
+        }
+        else if(Keyboard.current.sKey.isPressed)
+        {
+            Debug.Log("Pressing the S Key");
+        }
+        if(Keyboard.current.aKey.isPressed)
+        {
+            Debug.Log("Pressing the A Key");
+        }
+        else if(Keyboard.current.dKey.isPressed)
+        {
+            Debug.Log("Pressing the D Key");
+        }
+
+
         transform.Rotate(0,0,steerSpeed);
         transform.Translate(0, moveSpeed, 0);
     }
