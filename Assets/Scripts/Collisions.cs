64aab9733c0b2c42b8a945a2b592e8a4381c8c6c
tonevx <anthonysloat02@gmail.com>
2026-09-14 18:44:14 -0400

    Delivery Dash Part 1 Extra Add
    I've added a collectible set of coins that when collided with, adds onto a counter variable and prints the amount of coins you've gathered to the Unity console. Oh, and a tag for the coins too.

--- Assets/Scripts/Collisions.cs
+++ Assets/Scripts/Collisions.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class Collisions : MonoBehaviour
 {
+    float coinCounter = 0;
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Collision has occurred against the " + collision.gameObject.name + "!");
+        if (collision.collider.CompareTag("Coin"))
+        {
+            Destroy(collision.gameObject);
+            Debug.Log($"You collected a test Coin! It's... honestly kind of worthless. You now have {coinCounter} coins!");
+            coinCounter += 1;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
