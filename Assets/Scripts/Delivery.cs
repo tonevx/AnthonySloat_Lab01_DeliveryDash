1548c475a09fc105417eb90d934c61cf2f5aae99
tonevx <anthonysloat02@gmail.com>
2026-09-21 19:23:07 -0400

    Delivery Dash Lesson 16 (2.17)
    Added tags for package and customer, ensured that triggers work.

---
+++ Assets/Scripts/Delivery.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    float coinCounter = 1;
+    bool hasPackage = false;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Coin"))
+        {
+            Destroy(collision.gameObject);
+            Debug.Log($"You collected a test Coin! It's... honestly kind of worthless. You now have {coinCounter} coins!");
+            coinCounter += 1;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Package"))
+        {
+            if (hasPackage)
+            {
+                Debug.Log("You already have a package!!");
+                return;
+            }
+            hasPackage = true;
+            Debug.Log("Obtained Package!");
+            Destroy(collision.gameObject);
+        }
+        if (collision.CompareTag("Customer"))
+        {
+            if (hasPackage == false)
+            {
+                Debug.Log("You don't have a package!");
+            }
+            else
+            {
+                hasPackage = false;
+                Destroy(collision.gameObject);
+                Debug.Log("Package delivered!");
+            }
+        }
+    }
+}
